c502c14e045e0437a683eda7fa1367876393306d
Dann38 <38dunn@gmail.com>
2022-06-28 10:54:41 +0800

    - Вынес в блок создания canvas и text для изображения, обернул программу в класс

--- gui_read_img.py
+++ gui_read_img.py
@@ -2,86 +2,55 @@ from my_lib import *
 from tkinter import *
 from PIL import Image, ImageTk
 import cv2 as cv
-import os
-from sys import argv
-from pathlib import Path
 
 
-def main(name_file):
-    dir_ = os.getcwd()
-    f = os.path.join(dir_, name_file)
-    img = read_img(f)
+class App:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("GUI на Python")
+        self.root.geometry("1600x800")
+        img = get_img()
+        height, width = img.shape[:2]
+        self.WIDTH = 900
+        self.HEIGHT = self.WIDTH * height // width
 
-    height, width = img.shape[:2]
-    WIDTH = 900
-    HEIGHT = WIDTH * height // width
+        only_text = image_processing(img)
 
-    only_text = image_processing(img)
+        rez1, img = get_info_from_img(img)
+        rez2, only_text = get_info_from_img(only_text)
+        self.imgs = [img, only_text]
 
-    rez1, img = get_info_from_img(img)
-    rez2, only_text = get_info_from_img(only_text)
+        self.img_and_text_gui_block(rez1, 0)
+        self.img_and_text_gui_block(rez2, 1)
 
-    # Графический интерфейс =====================================================
-    root = Tk()
-    root.title("GUI на Python")
-    root.geometry("1600x800")
+        self.root.mainloop()
 
-    # Картинка до ===============================================================
-    canva1 = Canvas(root, width=WIDTH//2, height=HEIGHT//2, bg="gray")
-    canva1.pack(side=LEFT)
+    def img_and_text_gui_block(self, text_img, index_img):
+        canva = Canvas(self.root, width=self.WIDTH // 2, height=self.HEIGHT // 2, bg="gray")
+        canva.pack(side=LEFT)
 
-    canvawidth = int(canva1.winfo_reqwidth())
-    canvaheight = int(canva1.winfo_reqheight())
+        canvawidth = int(canva.winfo_reqwidth())
+        canvaheight = int(canva.winfo_reqheight())
 
-    img = cv.resize(img, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
+        self.imgs[index_img] = self.array_to_image_tk(self.imgs[index_img], canvawidth, canvaheight)
+        canva.create_image(0, 0, image=self.imgs[index_img], anchor='nw')
 
-    imgCV2 = cv.cvtColor(img, cv.COLOR_BGR2RGBA)  # Convert color from BGR to RGBA
-    current_image = Image.fromarray(imgCV2)  # Convert image into Image object
-    imgTK1 = ImageTk.PhotoImage(image=current_image)  # Convert image object to imageTK object
-    canva1.create_image(0, 0, anchor=NW, image=imgTK1)
+        text = Text(self.root, width=40, height=30)
+        text.pack(side=LEFT)
 
-    text1 = Text(root, width=40, height=30)
-    text1.pack(side=LEFT)
-    scroll1 = Scrollbar(command=text1.yview)
-    scroll1.pack(side=LEFT, fill=Y)
-    text1.config(yscrollcommand=scroll1.set)
-    text1.insert('1.0', rez1)
+        scroll = Scrollbar(command=text.yview)
+        scroll.pack(side=LEFT, fill=Y)
 
+        text.config(yscrollcommand=scroll.set)
+        text.insert('1.0', text_img)
 
-    # Картинка после ============================================================
-    canva2 = Canvas(root, width=WIDTH//2, height=HEIGHT//2, bg="gray")
-    canva2.pack(side=LEFT)
-
-    canvawidth = int(canva2.winfo_reqwidth())
-    canvaheight = int(canva2.winfo_reqheight())
-
-    only_text = cv.resize(only_text, (canvawidth, canvaheight), interpolation=cv.INTER_AREA)
-
-    imgCV2 = cv.cvtColor(only_text, cv.COLOR_BGR2RGBA)  # Convert color from BGR to RGBA
-    current_image = Image.fromarray(imgCV2)  # Convert image into Image object
-    imgTK2 = ImageTk.PhotoImage(image=current_image)  # Convert image object to imageTK object
-    canva2.create_image(0, 0, anchor=NW, image=imgTK2)
-
-    text2 = Text(root, width=40, height=30)
-    text2.pack(side=LEFT)
-    scroll2 = Scrollbar(command=text2.yview)
-    scroll2.pack(side=LEFT, fill=Y)
-    text2.config(yscrollcommand=scroll2.set)
-    text2.insert('1.0', rez2)
-
-
-    # Конец ======================================================================
-    root.mainloop()
+    @staticmethod
+    def array_to_image_tk(img, width, height):
+        img = cv.resize(img, (width, height), interpolation=cv.INTER_AREA)
+        img_cv = cv.cvtColor(img, cv.COLOR_BGR2RGBA)
+        current_image = Image.fromarray(img_cv)
+        return ImageTk.PhotoImage(current_image)
 
 
 if __name__ == '__main__':
-
-    if len(argv) > 1:
-        if argv[1] == "-h" or argv[1] == "--help":
-            print("Для запуска укажите путь до файла")
-        elif not Path(argv[1]).is_file():
-            print("Нет такого файла")
-        else:
-            main(argv[1])
-    else:
-        print("Для помощи вызовите команду -h или --help")
+    app = App()
